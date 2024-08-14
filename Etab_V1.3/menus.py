fe978736c3c470fc4eabfccc50c6fe03b7026496
Boystar225 <90429470+Boystar225@users.noreply.github.com>
2024-08-14 10:30:05 +0000

    Update menus.py

--- Etab_V1.3/menus.py
+++ Etab_V1.3/menus.py
@@ -2,28 +2,29 @@ from datetime import datetime
 import time
 
 # Constantes pour les couleurs
-jaune = '\033[0;93m'
-vert = '\033[0;92m'
-rouge = '\033[0;91m'
-cyan = '\033[0;96m'
-magenta = '\033[0;35m'
-reset = '\033[0m'
+# Constantes pour les couleurs
+bleu = '\033[0;94m'      # Bleu
+blanc = '\033[0;97m'     # Blanc
+gris = '\033[0;90m'      # Gris
+violet = '\033[0;95m'    # Violet
+turquoise = '\033[0;36m' # Turquoise
+reset = '\033[0m'        # Réinitialisation
 
 def accueil(contenu):
-    print(f"{jaune}******************************************************\n")
+    print(f"{bleu}******************************************************\n")
     print(f'\t{contenu}\n')
     print('******************************************************\033[0m')
 
 def afficher_menu():
-    print(f"{cyan}\n\tMENU")
+    print(f"{blanc}\n\tMENU")
     print("1: Gestion des élèves")
     print("2: Gestion des professeurs")
     print("3: Gestion des utilisateurs") 
     print("0: Quitter()\n" + reset)
-    print(f"Date et Heure Système : {magenta}{datetime.now().strftime('%d/%m/%Y  %H:%M')} \n{reset}")
+    print(f"Date et Heure Système : {gris}{datetime.now().strftime('%d/%m/%Y  %H:%M')} \n{reset}")
 
 def afficherSousMenuElev():
-    print(f"{cyan}\n\tMENU")
+    print(f"{violet}\n\tMENU")
     print("1: Ajouter un élève")
     print("2: Supprimer un élève")
     print("3: Modifier les informations de l'élève")
@@ -32,7 +33,7 @@ def afficherSousMenuElev():
     print("0: Accueil")
 
 def afficherSousMenuProf():
-    print(f"{cyan}\n\tMENU")
+    print(f"{violet}\n\tMENU")
     print("1: Ajouter un professeur")
     print("2: Supprimer un professeur")
     print("3: Modifier les informations du professeur")
@@ -41,7 +42,7 @@ def afficherSousMenuProf():
     print("0: Accueil")
 
 def afficherSousMenuUtil():
-    print(f"{cyan}\n\tMENU")
+    print(f"{violet}\n\tMENU")
     print("1: Ajouter un utilisateur")
     print("2: Supprimer un utilisateur")
     print("3: Modifier les informations d'un utilisateur")
@@ -53,5 +54,5 @@ def quitter(debut):
     fin = time.time()
     dure = fin - debut
     minutes, seconds = divmod(dure, 60)
-    print(f"{magenta}Merci d'avoir utilisé l'application ! À bientôt.")
+    print(f"{turquoise}Merci d'avoir utilisé l'application ! À bientôt.")
     print(f"Durée de la session : {int(minutes)} minutes et {int(seconds)} secondes.{reset}")
